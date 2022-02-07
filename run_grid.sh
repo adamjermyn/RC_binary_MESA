c6edb74afeac9685563e22002d455f722d79338a
Adam Jermyn <adamjermyn@gmail.com>
2022-02-07 10:51:59 -0500

    Updated meta

--- run_grid.sh
+++ run_grid.sh
@@ -3,7 +3,7 @@
 export MESASDK_ROOT="/mnt/home/ajermyn/Software/MESA/mesasdk"
 source $MESASDK_ROOT/bin/mesasdk_init.sh
 export MESA_DIR="/mnt/home/ajermyn/Software/MESA/mesa-r21.12.1"
-export OMP_NUM_THREADS=4
+export OMP_NUM_THREADS=20
 
 
 echo "Compiling..."
@@ -17,17 +17,13 @@ mkdir runs
 
 for j in 1.0 1.2 1.4 1.6 1.8 2.0
 do
-    for k in 0.005 0.01 0.015 0.02 0.03
-    do
-        rm -r runs/$j_$k
-        cp -R template runs/$j_$k    
-        cd runs/$j_$k
-        sed -i "s/MMM/$j/" inlist_project
-        sed -i "s/ZZZ/$k/" inlist_project
-        echo "Running Model..." $j_$k
-        ./rn &> rn.out &
-        cd ../../
-    done
+    rm -r runs/$j
+    cp -R template runs/$j    
+    cd runs/$j
+    sed -i "s/MMM/$j/" inlist_project
+    echo "Running Model..." $j
+    ./rn &> rn.out &
+    cd ../../
 done
 
 wait
